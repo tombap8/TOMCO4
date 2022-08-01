47ee4b82c4342bc42d0e55293b5fccf810e00122
Tom B. Parker <tombap8@hotmail.com>
2022-08-01 10:55:09 +0900

    Update main.js

--- 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -13,11 +13,27 @@ $(() => {
     // (3) 버튼들
     let btns = $(".btns button");
 
+    // (4) 메시지박스
+    let msg = $(".msg");
+
     // 1. 건물에 각방 번호 넣기
     bd.each((idx, ele) => { // idx-순번, ele-요소
         // text() 텍스트 넣기
         $(ele).text(idx);
+        // 9번방에 좀비넣기
+        if(idx===9) 
+            $(ele).append('<img src="images/mz1.png" alt="좀비1" class="mz">');
+        else if(idx===7) 
+            $(ele).append('<img src="images/mz2.png" alt="좀비2" class="mz">');
+        else if(idx===1) 
+            $(ele).append('<img src="images/zom.png" alt="좀비들" class="mz">');
+        else if(idx===2) 
+            $(ele).append('<img src="images/inj.png" alt="주사기" class="inj">');
     }); ///////// each ///////////////
+
+    // 좀비는 모두 숨기기: hide()
+    $(".mz").hide();
+
 
     // 2. 버튼 셋팅하기
     // 모든 버튼은 숨기고 첫번째 버튼만 보여
@@ -26,6 +42,13 @@ $(() => {
 
     // 3. "들어가기"버튼 클릭시
     btns.first().click(function () {
+
+        // 0. 버튼자신 없애기!
+        // 없애기 방법: hide(),slideUp(),fadeOut()
+        $(this).slideUp(300);
+        // + 메시지 없애기
+        msg.hide();
+
         // 1. 위치값 알아오기(대상:빌딩li)
         // 8번째 li
         // eq(순번) -> 순번요소 선택 메서드!
@@ -39,7 +62,18 @@ $(() => {
         mi.animate({
             top: tval+"px",
             left: lval+"px"
-        }, 1000, "easeInOutElastic")
+        }, 1000, "easeInOutElastic",
+        function(){ // 이동애니후
+
+            // 메시지 변경하기+보이기
+            msg
+            .text('와~! 아늑하다! 옆방으로 가보자!')
+            .show();
+
+            // 다음버튼 보이기
+            btns.eq(1).fadeIn(300);
+
+        }); /////// animate ///////
 
     }); ///////// click ///////////
 
