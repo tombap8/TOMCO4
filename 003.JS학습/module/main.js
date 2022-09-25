baf3dddf0516c71b849820d327bdfab2e3f89e69
tombraceparker <tombap8@hotmail.com>
2022-09-25 15:38:06 +0900

    Update main.js

--- 003.JS학습/module/main.js
+++ 003.JS학습/module/main.js
@@ -4,3 +4,5 @@ import message from "./message.js";
 document.getElementById("demo").innerHTML = message("현석",40);
 document.getElementById("demo").innerHTML += message("유나",25);
 document.getElementById("demo").innerHTML += message("성희",52);
+document.getElementById("demo").innerHTML += 
+`이것은 다른 출력입니다! 이름:${name} , 나이:${age}<br>`;
