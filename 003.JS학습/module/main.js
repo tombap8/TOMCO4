0fd6cff2f99595bd1060a20edc6bb9909b81fb5a
tombraceparker <tombap8@hotmail.com>
2022-09-25 15:48:47 +0900

    ss

--- 003.JS학습/module/main.js
+++ 003.JS학습/module/main.js
@@ -1,8 +1,16 @@
-import { name, age } from "./person.js";
+import { name, age, personInfo } from "./person.js";
 import message from "./message.js";
 
-document.getElementById("demo").innerHTML = message("현석",40);
-document.getElementById("demo").innerHTML += message("유나",25);
-document.getElementById("demo").innerHTML += message("성희",52);
-document.getElementById("demo").innerHTML += 
+const exp = document.getElementById("demo");
+
+exp.innerHTML = message("현석",40);
+exp.innerHTML += message("유나",25);
+exp.innerHTML += message("성희",52);
+exp.innerHTML += 
 `이것은 다른 출력입니다! 이름:${name} , 나이:${age}<br>`;
+
+// 다중데이터를 이용해보자!
+
+personInfo.forEach((v)=>{
+    exp.innerHTML += message(v[0],v[1]);
+});
--- 003.JS학습/module/person.js
+++ 003.JS학습/module/person.js
@@ -1,8 +1,11 @@
 // export const name = "Jesse";
 // export const age = 40;
 
-
+const personInfo = 
+[
+    ["준현",23],["정희",33],["성락",45],["진경",22]
+];
 const name = "Jesse";
 const age = 40;
 
-export {name, age};
+export {personInfo, name, age};
